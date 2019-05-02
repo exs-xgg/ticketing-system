19a6efa91c31a2bb0a5cba58d265a8918baf62f6
cleidelacruzx <47548449+cleidelacruzx@users.noreply.github.com>
2019-05-02 14:02:38 +0800

    new

---
+++ resources/views/student/concern/create.blade.php
@@ -0,0 +1,122 @@
+@extends('layouts.app')
+
+@section('styles')
+<link href="{{ asset('css/select2.css') }}" rel="stylesheet">
+<style>
+    .select2-results{
+        border: 1px solid #ced4da !important;
+    }
+</style>
+@endsection
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="post-prev-title">
+                <h3>Concerns</h3>
+            </div>
+            <hr class="mt-3">
+        </div>
+    </div>
+    <div class="row mt-3 justify-content-center">
+        <div class="col-lg-8">
+            <div class="card">
+                <div class="card-header text-white bg-primary">
+                    <h5 class="text-oswald mb-0">Add Concern</h5>
+                </div>
+                <div class="card-body">
+                    <form action="{{route('student.concern.store')}}" method="post">
+                        {{ csrf_field() }}
+        
+                        <div class="md-form">
+                             <select class="select-wrapper mdb-select" name="prob_category" id="prob_category">
+                                  <option value="" selected>Select</option>
+                                  <option value="Technical" {{ old('prob_category') == 'Technical' ? 'selected' : ''}}>Technical</option>
+                                  <option value="PCB" {{ old('prob_category') == 'PCB' ? 'selected' : ''}}>PCB</option>
+                                  <option value="Eclaims" {{ old('prob_category') == 'Eclaims' ? 'selected' : ''}}>Eclaims</option>
+                                 <option value="HPP" {{ old('prob_category') == 'HPP' ? 'selected' : ''}}>HPP</option>        
+                              </select>
+                                <label for="prob_category">Problem Category</label>
+                            </div>
+
+                         <div class="md-form">
+                             <select class="select-wrapper mdb-select" name="sub_category" id="sub_category">
+                                  <option value="" selected>Select</option>
+                                  <option value="Network/Connection" {{ old('sub_category') == 'Network/Connection' ? 'selected' : ''}}>Technical- Network/Connection</option>
+                                  <option value="Server/Backup" {{ old('sub_category') == 'Server/Backup' ? 'selected' : ''}}>Technical- Server/Backup</option>
+                                  <option value="Performance Issue" {{ old('sub_category') == 'Performance Issue' ? 'selected' : ''}}>Technical- Performance Issue</option>
+                                 <option value="Submission Statistics" {{ old('sub_category') == 'Submission Statistics' ? 'selected' : ''}}>PCB- Submission Statistics</option>        
+                
+                              <option value="Resubmission Request" {{ old('sub_category') == 'Resubmission Request' ? 'selected' : ''}}>PCB- Resubmission Request</option>        
+                              
+                               <option value="Cipher Keys" {{ old('sub_category') == 'Cipher Keys' ? 'selected' : ''}}>Eclaims- Cipher Keys</option>        
+                              
+                              <option value="Error Encountered" {{ old('sub_category') == 'Error Encountered' ? 'selected' : ''}}>Eclaims- Error Encountered</option>        
+                              
+                              <option value="Module 2 Training" {{ old('sub_category') == 'Module 2 Training' ? 'selected' : ''}}>HPP- Module 2 Training</option>        
+                              
+                              <option value="Module 1 Training" {{ old('sub_category') == 'Module 1 Training' ? 'selected' : ''}}>HPP- Module 1 Training</option>        
+                              
+                              <option value="Module 1&2 Training" {{ old('sub_category') == 'Module 1&2 Training' ? 'selected' : ''}}>HPP- Module 1&2 Training</option>        
+                            
+                               <option value="New Site" {{ old('sub_category') == 'New Site' ? 'selected' : ''}}>HPP- New Site</option>        
+                              
+                              <option value="Refresher Training" {{ old('sub_category') == 'Refresher Training' ? 'selected' : ''}}>HPP- Refresher Training</option>        
+                              </select>
+                                <label for="sub_category">Sub-Category</label>
+                        </div>
+
+        
+                        <div class="form-group">
+                            <label class="select2Label">Problem</label>
+                            <textarea type="text" id="problem" name="problem" rows="5" class="form-control rounded-0 {{$errors->has('problem') ? 'is-invalid' : ''}}">{{old('problem')}}</textarea>
+                            @if ($errors->has('problem'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('problem') }}</strong>
+                            </span>
+                            @endif
+                        </div>
+
+                            <div class="form-group">
+                            <label class="select2Label">What happened before encountering the problem?</label>
+                            <textarea type="text" id="before" name="before" rows="5" class="form-control rounded-0 {{$errors->has('before') ? 'is-invalid' : ''}}">{{old('before')}}</textarea>
+                            @if ($errors->has('before'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('before') }}</strong>
+                            </span>
+                            @endif
+                        </div>
+
+                             
+                                    <input type="text" hidden="" value="{{ Auth::user()->id }}" name="reporter" id="reporter">
+                          
+                        
+
+                        
+
+        
+                        <button type="submit" name="button" class="btn btn-primary float-right mt-4"><i class="fa fa-save"></i> Save</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('script')
+<script src="{{ asset('js/select2.min.js') }}"></script>
+<script>
+    $('.mdb-select').material_select();
+    $('.multiple-select').select2();
+    $('.multiple-select').select2().val({!! json_encode(old('admins')) !!}).trigger('change');
+    $('.datepicker').pickadate({
+        max: new Date(),
+        formatSubmit: 'yyyy-mm-dd',
+        hiddenPrefix: 'formatted_',
+        selectYears: 50
+    });
+
+</script>
+@endsection
---
+++ resources/views/student/concern/index.blade.php
@@ -0,0 +1,203 @@
+
+@extends('layouts.app')
+
+@section('styles')
+<link href="{{ asset('css/addons/datatables.min.css') }}" rel="stylesheet">
+
+<style>
+         pre {
+            overflow-x: auto;
+            white-space: pre-wrap;
+            white-space: -moz-pre-wrap;
+            white-space: -pre-wrap;
+            white-space: -o-pre-wrap;
+            word-wrap: break-word;
+         }
+            
+          
+                                
+                   /* .level1{
+                        color:  !important;
+                    }
+                    .level2{
+                        color: !important;
+                    }
+                    .level3{
+                        color: !important;
+                    }
+*/
+
+
+      </style>
+@endsection
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-12 d-flex justify-content-between">
+            <div class="post-prev-title">
+                <h3>Concerns</h3>
+            </div>
+            <a href="{{route('student.concern.create')}}" class="btn btn-primary mr-0 my-0"><i class="fa fa-plus"></i> Add concern</a>
+        </div>
+    </div>
+    <hr class="mt-2">
+    <div class="row mt-3">
+        <div class="col-lg-4 col-md-4 col-sm-6 mb-3">
+            <div class="card">
+                <div class="text-white blue text-center py-4 px-4">
+                    <i class="fa fa-list fa-3x tiles-left-icon"></i>
+                    <h2 class="card-title pt-2 text-white text-oswald"><strong>{{ number_format(count($concerns) )}}</strong></h2>
+                    <h2 class="text-uppercase text-white text-oswald">Concern{{ count($concerns) > 1 ? 's' : '' }}</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-3">
+        <div class="col-xl-12 col-md-12 mb-4">
+            <div class="card">
+                <div class="card-body pb-0">
+                    <table id="table" class="table text-nowrap" cellspacing="0" width="100%">
+                        <thead>
+                            <tr>
+                                <th class="th-sm">Ticket#</th>
+                                <th class="th-sm">Date</th>
+                                <th class="th-sm"> Report to</th>
+                                 <th class="th-sm"> Endorse to </th>
+                                <th class="th-sm">Problem Category</th>
+                                <th class="th-sm">Sub Category</th>
+                                <th class="th-sm">Priority</th>
+                                <th class="th-sm">Status</th>
+                                <th class="th-sm">Problem</th>
+                                <th class="th-sm">Before Problem</th>
+                                <th class="th-sm">Remarks</th>
+                                <th class="th-sm">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+<!-- 
+                            <?php
+                            echo($concerns)
+                            ?> -->
+
+                              @foreach ($concerns as $data)
+                                 <tr>
+                                <td>{{$data->ticket}}</td>
+                                <td>{{$data->created_at}}</td>
+
+                        
+                                 <td>
+
+                                   <!--  @foreach ($data->users as $key => $user)
+
+                                      <a>{{ $user->name() }}</a>
+                                          {{ $key < count($data->users) - 1 ? ', ' : ''  }}
+                                        @endforeach   -->
+                                    {{$data->firstName}}  {{$data->lastName}}
+                                  </td>   
+                                   <td>
+                                  <!--   @foreach ($data->users as $key => $user)
+                                      <a>{{ $user->name() }}</a>
+                                          {{ $key < count($data->users) - 2 ? ', ' : ''  }}
+
+                                        @endforeach   -->
+                                         {{ \App\User::where('id', $data->receiver2)->value('firstName') }}
+                                        {{ \App\User::where('id', $data->receiver2)->value('lastName') }}
+                                  </td>   
+
+
+                                <td>{{$data->prob_category}}</td>
+                                <td>{{$data->sub_category}}</td>
+
+                                <td
+                                 class="
+                                @if($data->priority == 'level 1') blue
+                                @elseif($data->priority == 'level 2') yellow 
+                                @elseif($data->priority == 'level 3') amber
+                                @endif">
+                                {{ $data->priority }}</td>
+
+                                <td>{{$data->status}}</td>
+                                 <td><pre>{{$data->problem}}<pre></td>
+                                <td><pre>{{$data->before}}<pre></td>
+                                <td>{{$data->remark}}</td>
+
+                                <td>
+                                    <a href="{{route('student.concern.edit', $data->id)}}" class="blue-text mr-3" data-toggle="tooltip" title="Edit" data-placement="left"><i class="fa fa-pencil"></i></a>
+                                    <a href="javascript:void(0);" data-href="{{ route('student.concern.destroy', $data->id) }}" class="anchor_delete text-danger" data-method="delete" data-action="concern" data-from="concern" data-toggle="tooltip" title="Delete" data-placement="right"><i class="fa fa-trash"></i></a> 
+                                </td>
+                            </tr>    
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('script')
+<script src="{{ asset('js/addons/datatables.min.js') }}"></script>
+<script>
+    $(document).ready(function () {
+        $('#table').DataTable({
+            scrollX: true,
+            // processing: true,
+            // serverSide: true,
+            // ajax: "{!! route('admin.courseList') !!}",
+            // columns: [
+            //     {data: 'name', name: 'name'},
+            //     {data: 'instructors', name: 'instructors'},
+            //     {data: 'status', name: 'status'},
+            //     {data: 'action', name: 'action'},
+            // ],
+            language: {
+                search: "_INPUT_",
+                searchPlaceholder: "Search",
+            },
+            order: [],
+        });
+        // tooltip for datatables
+        $('table').on('draw.dt', function() {
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+
+        $('#table').on('change', '.active-mode-switch', function() {
+            var status = 0;
+            var id = $(this).attr('courseId');
+            if ($(this).is(':checked')) {
+                status = 1;
+            }
+
+            $.ajax({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                url: "/admin/course/"+id+"/status",
+                type : 'PUT',
+                data: { id: id, status : status },
+                success: function(result) {
+                    var newResult = JSON.parse(result);
+                    const toast = swal.mixin({
+                        toast: true,
+                        position: 'top-end',
+                        showConfirmButton: false,
+                        timer: 3000
+                    });
+
+                    toast({
+                        type: 'success',
+                        title: newResult.status
+                    })
+                },
+                error : function(error) {
+                    console.log('error');
+                    console.log(error);
+                }
+            });
+        });
+    });
+</script>
+@endsection
